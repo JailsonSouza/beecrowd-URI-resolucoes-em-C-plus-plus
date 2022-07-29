0c5fd2fd0b69669bf32abb7252a35fa9f81457fd
Jailson Souza Carvalho <jailsonsouzaa714@gmail.com>
2022-07-28 23:23:46 -0300

    Upando os Códigos

--- Todas as Questões/teste.c
+++ Todas as Questões/teste.c
@@ -1,25 +1,9 @@
-a, b, c = input().split(" ")
-a = float(a)
-b = float(b)
-c = float(c)
-if ((b>a)and(b>c)):
-    aux = (b)
-    b = (a)
-    a = (aux)
-if ((c>a)and(c>b)):
-    aux = (c)
-    c = (a)
-    a = (aux)
-    
-if (a >= (b+c)):
-    print('NAO FORMA TRIANGULO')
-elif ((a*a) == ((b*b)+(c*c))):
-    print('TRIANGULO RETANGULO')
-elif ((a*a) > ((b*b)+(c*c))):
-    print('TRIANGULO OBTUSANGULO')
-if ((a*a) < ((b*b)+(c*c))):
-    print('TRIANGULO ACUTANGULO') 
-if ((a == b) and (b == c) and(c == a)):
-    print('TRIANGULO EQUILATERO')
-elif (((a == b) and (b == a)) or ((b == c) and (c == b)) or ((c == a) and (a == c))):
-    print('TRIANGULO ISOSCELES')
+hi, hf = input().split(" ")
+hi = int(hi)
+hf = int(hf)
+if (hi > hf):
+  print('O JOGO DUROU',((24 - hi) + hf),'HORA(S)')
+elif (hi < hf):
+  print('O JOGO DUROU',(hf-hi),'HORA(S)')
+else:
+  print('O JOGO DUROU 24 HORA(S)')
